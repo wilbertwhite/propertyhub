cd093f99e10642312852218b3f35d3df4d8ea636
ERuiz30 <edwin.ruiz93@yahoo.com>
2022-07-30 23:22:19 -0400

    removal of useless code
    removal of useless code in login.php

--- login.php
+++ login.php
@@ -3,18 +3,9 @@ include('dbconfig.php');
 session_start();
 unset($_SESSION["account_success"]);
 
-//This is where information will need to be updated to connect to mysql on CODD
-$host = "localhost";
-$user = "eruiz11";
-$password = "eruiz11";
-$dbname = "eruiz11";
-
-$con = new mysqli($host, $user, $password, $dbname);
-
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
-echo "Connected successfully!";
 
 // get the post records
 $txtUsername = $_POST['username'];
