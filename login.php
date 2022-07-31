e6a152abfe73616a942dc2b8e82871b0e8a6e2b8
wilbertwhite <wilbertcwhite@gmail.com>
2022-07-30 23:50:43 -0400

    added login

--- login.php
+++ login.php
@@ -21,11 +21,32 @@ $txtUsername = $_POST['username'];
 $txtPassword = $_POST['password'];
 
 //find username in database according to what user entered
-$sql = "SELECT username FROM LOGININFO WHERE username = '$txtUsername'";
+$sql = "SELECT * FROM LOGININFO WHERE username = '$txtUsername'";
 $rs = mysqli_query($con, $sql);
 $row = mysqli_fetch_assoc($rs);
+$row_password = $row["password"];
+
+$sql3 = "SELECT * FROM LOGININFO WHERE password = password('$txtPassword')";
+$rs3 = mysqli_query($con, $sql3);
+$row3 = mysqli_fetch_assoc($rs3);
+$row3_password = $row3["password"];
 
 //compare username entered to the query result
 if ($txtUsername == $row["username"]) {
     echo ("username found");
+    echo ($row["username"]);
+    if ($row3_password == $row_password) {
+        if ($row["accountType"] == "seller") {
+            header("Location: sellindex.php");
+            exit();
+        } else if ($row["accountType"] == "buyer") {
+            header("Location: buy.php");
+            exit();
+        }
+    }
+    echo ("entered password :" . $row3_password);
+    echo ("oldpassword(rowpassword:) :" . $row_password);
+} else {
+    header("Location: loginindex.php");
+    exit();
 }
